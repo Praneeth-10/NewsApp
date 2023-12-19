44e5ddc632a35a85a0f4c08cfee6468c61404dbf
Praneeth-10 <sp.parasa@gmail.com>
2023-12-18 22:38:48 -0600

    Updating the dependencies and versions.

--- buildSRC/src/main/java/Dependencies.kt
+++ buildSRC/src/main/java/Dependencies.kt
@@ -1,5 +1,8 @@
 object Dependencies {
 
     val coreKtx by lazy {   "androidx.core:core-ktx:${Versions.coreKtx}"    }
+    val lifecycleRuntimeKtx by lazy { "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleRuntimeKtx}" }
+    val activityCompose by lazy { "androidx.activity:activity-compose:${Versions.activityCompose}" }
+    val composeBom by lazy { "androidx.compose:compose-bom:${Versions.composeBom}"}
 
 }
--- buildSRC/src/main/java/Versions.kt
+++ buildSRC/src/main/java/Versions.kt
@@ -1,3 +1,7 @@
 object Versions {
     const val coreKtx = "1.12.0"
+    const val lifecycleRuntimeKtx = "2.6.2"
+    const val activityCompose = "1.8.1"
+    const val composeBom = "2023.08.00"
+
 }
