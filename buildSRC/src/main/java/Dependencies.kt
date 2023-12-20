52d0eeabe4bebf1e456b537065dc3419564ad345
Praneeth-10 <sp.parasa@gmail.com>
2023-12-19 23:00:42 -0600

    Updating the dependencies and versions for the buildSRC module.

--- buildSRC/src/main/java/Dependencies.kt
+++ buildSRC/src/main/java/Dependencies.kt
@@ -4,5 +4,11 @@ object Dependencies {
     val lifecycleRuntimeKtx by lazy { "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleRuntimeKtx}" }
     val activityCompose by lazy { "androidx.activity:activity-compose:${Versions.activityCompose}" }
     val composeBom by lazy { "androidx.compose:compose-bom:${Versions.composeBom}"}
+    val composeUi by lazy { "androidx.compose.ui:ui" }
+    val composeUiGraphics by lazy { "androidx.compose.ui:ui-graphics:" }
+    val composeUiToolingPreview by lazy { "androidx.compose.ui:ui-tooling-preview" }
+    val composematerial3 by lazy { "androidx.compose.material3:material3" }
+    val navigationCompose by lazy { "androidx.navigation:navigation-compose:"}
+    val lifecycleViewModelKtxCompose by lazy { "androidx.lifecycle:lifecycle-viewmodel-compose:" }
 
 }
